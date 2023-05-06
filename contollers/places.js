802d21fdacf114e0536a186d7aac8a634d47091f
wbrewster02 <wbrewster02@gmail.com>
2023-05-06 11:46:50 -0400

    adding and rendering view

--- contollers/places.js
+++ contollers/places.js
@@ -1,20 +1,33 @@
 const router = require('express').Router()
 
 // More code here in a moment
+
+// GET /places
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/coffeeShop.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/cafe.jpg'
-      }]
+  let places = []
+  res.render('places/index')
+})
+
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+
+router.get('/', (req, res) => {
+  let places = [{
+      name: 'H-Thai-ML',
+      city: 'Seattle',
+      state: 'WA',
+      cuisines: 'Thai, Pan-Asian',
+      pic: '/images/coffeeShop.jpg'
+    }, {
+      name: 'Coding Cat Cafe',
+      city: 'Phoenix',
+      state: 'AZ',
+      cuisines: 'Coffee, Bakery',
+      pic: '/images/cafe.jpg'
+    }]
       
     res.render('places/index', { places })
 })
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,20 +2,22 @@ const React = require('react')
 const Def = require('../default')
 
 function index (data) {
-    let placesFormatted = data.places.map((place, i) => {
-      return (
-        <div key={i} className="col-sm-6">
-          <h2>{place.name}</h2>
-          <p className="text-center">
-            {place.cuisines}
-          </p>
-          <img src={place.pic} alt={place.name}/>
-          <p className="text-center">
-            Located in {place.city}, {place.state}
-          </p>
-        </div>
-      )
-    })
+  let placesFormatted = data.places.map((place, i) => {
+    return (
+      <div key={i} className="col-sm-6">
+        <h2>
+          {place.name}
+        </h2>
+        <p className="text-center">
+          {place.cuisines}
+        </p>
+        <img src={place.pic} alt={place.name}/>
+        <p className="text-center">
+          Located in {place.city}, {place.state}
+        </p>
+      </div>
+    )
+})
     return (
       <Def>
         <main>
