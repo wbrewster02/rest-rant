9ce5d7880573a1d2b2b19f9d0e36bfb6eb0dcd49
wbrewster02 <wbrewster02@gmail.com>
2023-05-20 17:07:14 -0400

    finished?

--- controllers/places.js
+++ controllers/places.js
@@ -41,23 +41,72 @@ router.get('/:id', (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
+  db.Places.findByIdAndUpdate(req.params.id, req.body)
+  .then(() => {
+    res.status(303).redirect(`/places/${req.params.id}`)
+  })
+  .catch(err => {
+    console.log('err', err)
+    res.status(404).render('error404')
+  })
 })
 
 router.delete('/:id', (req, res) => {
-  res.send('DELETE /places/:id stub')
+  db.Places.findByIdAndDelete(req.params.id)
+  .then(place => {
+    res.status(303).redirect('/places')
+  })
+  .catch(err => {
+    console.log('err', err)
+    res.status(404).render('error404')
+  })
 })
 
 router.get('/:id/edit', (req, res) => {
-  res.send('GET edit form stub')
+  db.Places.findById(req.params.id)
+  .then(place => {
+    res.render('../views/places/edit', { place })
+  })
+  .catch(err => {
+    res.status(404).render('error404', err)
+  })
 })
 
 router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
+  db.Places.findById(req.params.id)
+  .then(place => {
+    db.Comment.create(req.body)
+    .then(comment => {
+      place.comments.push(comment.id)
+      place.save()
+      .then(() => {
+        res.redirect(`/places/${req.params.id}`)
+      })
+      .catch(err=>{
+        console.log(err)
+        res.status(404).render('error404')
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.render('error404')
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.render('error404')
+  })
 })
 
 router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('GET /places/:id/rant/:rantId stub')
+  db.Comment.findByIdAndDelete(req.params.rantId)
+  .then(place => {
+    res.status(303).redirect(`/places/${req.params.id}`)
+  })
+  .catch(err => {
+    console.log('err', err)
+    res.status(404).render('error404')
+  })
 })
 
 module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,28 @@ function Show (data) {
             No comments yet!
         </h3>
     )
-    {if (data.place.comments.length) {
+    //if there are no raitings yet this is displayed
+    let rating = (
+        <h3 className="inactive">
+            Not Yet rated
+        </h3>
+    )
+    if (data.place.comments.length) {
+        //gets the total star rating then gets the average and displays the average
+        let sumRatings = data.place.comments.reduce((tot,c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = Math.round(sumRatings / data.place.comments.length)
+        let stars = ''
+        for (let i = 0; i< averageRating; i++) {
+            stars += '⭐'          
+        } 
+        rating = (
+            <h3>
+                {stars} stars
+            </h3>
+        )}
+    if (data.place.comments.length) {
         comments = data.place.comments.map(c => {
         return (
             <div className="border">
@@ -17,10 +38,13 @@ function Show (data) {
                 <stong>- {c.author}</stong>
             </h3>
             <h4>Rating: {c.stars}</h4>
+                <form method="POST" action={`/places/${data.place.id}/rant/${c.id}?_method=DELETE`}>
+                    <input type="submit" className="btn btn-danger" value="Delete Comment" />
+                </form>
             </div>
         )
         })
-    }}
+    }
     return(
         <Def>
             <main>
@@ -42,10 +66,10 @@ function Show (data) {
                         <h4>
                             Serving {data.place.cuisines}
                         </h4>
-                        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                        <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
                           Edit
                         </a>
-                        <form method="post" action={`/places/${data.id}?_method=DELETE`}>
+                        <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
                             <button type="submit" className="btn btn-danger">
                                 Delete
                             </button>
@@ -54,9 +78,20 @@ function Show (data) {
                 </div>
                 <div className="row">
                     <h2>Comments</h2>
-                    <p>
                         {comments}
-                    </p>
+                    <form method="post" action={`/places/${data.place.id}/rant`}>
+                        <label className="form-group" htmlFor="author" >Author</label>
+                        <input className="form-control" type="text" id="author" name="author" />
+                        <label className="form-group" htmlFor="Content" >Content</label>
+                        <input className="form-control" type="textarea" id="content" name="content" />
+                        <label className="form-group" htmlFor="stars" >StarRating</label>
+                        <input className="form-control" type="Number" id="stars" name="stars" min="0" max="5" step=".5" placeholder="0-5 stars by .5" />
+                        <label  htmlFor="rant">rant</label>
+                        <input  type="checkbox" id="rant" name="rant" value="true"/>
+                        <br></br>
+                        <br></br>
+                        <input className="btn btn-primary" type="submit" value="add comment" />
+                    </form>
                 </div>
             </main>
         </Def>
